74db9af39cb25ce81b9f5afb8740a39ba221cfc8
Chris <52128256+cchriso@users.noreply.github.com>
2020-07-19 21:39:26 +0200

    Create script.py

---
+++ script.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+import telegram, time
+import logging, sys
+
+# debug true/false
+logging.basicConfig(stream=sys.stdout, level=logging.ERROR,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("noegig")
+logger.setLevel(logging.INFO)
+
+baseurl = "https://www.noegig.at/bestellung/"
+token = ""
+chat_id = 0
+
+
+# telegram
+def send_to_telegram(message):
+    bot = telegram.Bot(token)
+    text = "found new Gemeinden for noegig: {}, here is the link to the new page: {}".format(message,baseurl)
+    bot.sendMessage(chat_id=chat_id, text=text, disable_web_page_preview=True)
+
+
+def get_new_list():
+    list_gemeinden = []
+    s = requests.Session()
+    site = s.get(url=baseurl)
+    soup = BeautifulSoup(site.text, features="html.parser")
+    for y in soup.find_all('p', class_='toggler'):
+        list_gemeinden.append(y.text)
+    return list_gemeinden
+
+
+def main():
+    old_list_gemeinden = get_new_list()
+
+    logger.info("Bot started!")
+
+    while True:
+        neue_gemeinden = []
+        new_list_gemeinden = get_new_list()
+        for element in new_list_gemeinden:
+            if element not in old_list_gemeinden:
+                neue_gemeinden.append(element)
+
+        # check if list has entries (list not empty)
+        if neue_gemeinden:
+            send_to_telegram(neue_gemeinden)
+            logger.info("Found new Gemeinden, and sent it to Telegram Notification Channel")
+
+        old_list_gemeinden = new_list_gemeinden
+
+        time.sleep(300)
+        logger.info("Neuer Durchgang")
+
+
+if __name__ == '__main__':
+    main()
